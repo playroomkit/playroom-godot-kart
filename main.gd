da0f139c302b770a56b6f7040bf3267a73e17e77
Daichi <daichi@murokami.com>
2024-01-02 14:42:14 -0800

    initial commit

---
+++ main.gd
@@ -0,0 +1,12 @@
+
+# entry point for the program on startup
+extends Node
+
+@export var change_scene_to : PackedScene
+
+func _ready():
+	
+	# switch immediately to the menu level
+	get_tree().change_scene_to_packed(change_scene_to)
+	
+	pass
