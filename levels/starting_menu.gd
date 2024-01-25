3c9e324e44050aff3c7b7211785a1cf10437ee68
Daichi <daichi@murokami.com>
2024-01-24 18:13:59 -0800

    save before attempting to create pseudo-stream mode and cross platform

--- levels/starting_menu.gd
+++ levels/starting_menu.gd
@@ -10,7 +10,10 @@ extends Control
 
 @export var controller_scene : PackedScene
 @export var race_scene : PackedScene
-@export var debug = false
+
+var stream_mode = false
+var debug = false
+var remote = false
 
 var playroom : PlayroomInstance
 
@@ -22,7 +25,9 @@ var playroom : PlayroomInstance
 
 func _ready():
 	
+	# get global playroom instance
 	playroom = Playroom.instance
+	
 	# connect to the playroom instance signal to listen for game start -
 	# done in code since the playroom global isn't placed in the tree until runtime
 	playroom.coin_inserted.connect(_on_playroom_coin_inserted)
@@ -30,11 +35,28 @@ func _ready():
 
 # this callback was connected to a button through the editor
 func _on_button_pressed():
+	stream_mode = true
+	_start_playroom()
+
+
+func _on_button_remote_pressed():
+	remote = true
+	_start_playroom()
+
+
+func _on_button_debug_pressed():
+	debug = true
 	_start_playroom()
 
 
 func _on_playroom_coin_inserted(args):
-	_switch_scene()
+	
+	if debug: 
+		_switch_scene_debug()
+	elif remote: 
+		_switch_scene_remote()
+	else: 
+		_switch_scene()
 
 
 
@@ -43,20 +65,26 @@ func _on_playroom_coin_inserted(args):
 
 
 func _start_playroom():
-	if debug: Playroom.instance.stream_mode = false
+	Playroom.instance.stream_mode = stream_mode
 	Playroom.instance.start_playroom()
 
 
+# for stream mode - sets up game as main screen + controllers
 func _switch_scene():
-	
-	if debug:
-		if playroom.playroom_is_host(): 
-			get_tree().change_scene_to_packed(race_scene)
-		else:
-			get_tree().change_scene_to_packed(controller_scene)
-	
+	if playroom.playroom_is_stream_screen():
+		get_tree().change_scene_to_packed(race_scene)
 	else:
-		if playroom.playroom_is_stream_screen():
-			get_tree().change_scene_to_packed(race_scene)
-		else:
-			get_tree().change_scene_to_packed(controller_scene)
+		get_tree().change_scene_to_packed(controller_scene)
+
+
+# gives controls to the main scene
+func _switch_scene_debug():
+	if playroom.playroom_is_host(): 
+		get_tree().change_scene_to_packed(race_scene)
+	else:
+		get_tree().change_scene_to_packed(controller_scene)
+
+
+
+func _switch_scene_remote():
+	pass
